5cb7e65075cef3156daa007818e1d694f72c2cea
mochi-yu <mochi.2225.it@gmail.com>
2023-11-16 22:30:34 +0900

    feat: サンプルデータでの実装

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -5,12 +5,57 @@ import { NewsList } from '@/app/(components)/(news)/NewsList';
 import { News } from '@/app/(model)/news';
 
 export default function TopPage() {
-  const schedules: Schedule[] = [];
-  const newses: News[] = [];
+  const today = new Date();
+  const sampleSchedules: Schedule[] = [
+    {
+      title: '予定の内容',
+      startDay: new Date(today.setDate(today.getDate() + 1)),
+      endDay: new Date(today.setDate(today.getDate() + 10)),
+    },
+    {
+      title: '予定の内容',
+      startDay: new Date(today.setDate(today.getDate() + 1)),
+      endDay: undefined,
+    },
+    {
+      title: 'ながーーーーーーーーーーーーーい予定の内容',
+      startDay: undefined,
+      endDay: undefined,
+    },
+  ];
+  const sampleNewses: News[] = [
+    {
+      imageUrl: 'https://avatars.githubusercontent.com/u/43375000',
+      title: '記事タイトル１',
+      summary: '記事の要約',
+      postDate: today,
+    },
+    {
+      imageUrl: undefined,
+      title: '記事タイトル２',
+      summary: '記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約',
+      postDate: today,
+    },
+    {
+      imageUrl: 'https://avatars.githubusercontent.com/u/43375000',
+      title: '記事タイトル３',
+      summary: '記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約',
+      postDate: today,
+    },
+    {
+      imageUrl: undefined,
+      title: '記事タイトル４',
+      summary: '記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約記事の要約',
+      postDate: today,
+    },
+  ];
+
+  const schedules: Schedule[] = sampleSchedules;
+  const newses: News[] = sampleNewses;
 
   return (
     <>
-      <Stack p="30px" width="100%" spacing={2}>
+      <Stack p="30px" width="100%" spacing="25px">
         <Typography fontSize={24}>直近の予定</Typography>
         <ScheduleList schedules={schedules} />
 
