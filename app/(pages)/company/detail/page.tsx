e390bf3f38ada804c23fd5719a467feb3c6291ea
mochi-yu <mochi.2225.it@gmail.com>
2023-11-18 04:39:59 +0900

    wip

---
+++ app/(pages)/company/detail/page.tsx
@@ -0,0 +1,9 @@
+import { Stack } from '@mui/material';
+
+export default function CompanyDetailPage() {
+  return (
+    <>
+      <Stack p="30px" width="100%" spacing="25px"></Stack>
+    </>
+  );
+}
