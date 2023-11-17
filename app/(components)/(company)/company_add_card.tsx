bd746724ece7f47a9df57895d6c83797024e43c1
mochi-yu <mochi.2225.it@gmail.com>
2023-11-17 20:35:12 +0900

    fix: 全てクリックできるように修正

--- app/(components)/(company)/company_add_card.tsx
+++ app/(components)/(company)/company_add_card.tsx
@@ -11,8 +11,8 @@ export function CompanyAddCard() {
     <>
       <Card sx={{ borderRadius: '20px', boxShadow: 0, height: '150px', border: 'dashed 2px' }}>
         <CardActionArea onClick={clickHandler} sx={{ height: '100%' }}>
-          <Link href="/company/add" style={{ color: '#000' }}>
-            <Stack alignItems="center" justifyContent="center">
+          <Link href="/company/add" style={{ color: '#000', height: '100%' }}>
+            <Stack alignItems="center" justifyContent="center" height="100%">
               <Add />
             </Stack>
           </Link>
